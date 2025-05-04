7daf438e80717b18bac942d322b1a3238f6187f4
Emtiaz Ahmed <emtiazahmed20@gmail.com>
2025-05-04 20:15:41 +0600

    2nd Commit

--- src/pages/Subjects.jsx
+++ src/pages/Subjects.jsx
@@ -1,5 +1,5 @@
 function Subjects() {
-    return <h1 className="text-2xl font-bold">📚 Subjects Page</h1>;
+    return <h1 className="text-2xl font-bold">📚 Subjects Page....</h1>;
   }
   export default Subjects;
   
